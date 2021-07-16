679621f7a8efb8a0164d40217d251e1ba6610bcf
nugowe <nosa.ugowe@yahoo.com>
2021-07-16 17:43:57 -0500

    Rename Uefalogo.R to euro2020logo.R

--- Uefalogo.R
+++
@@ -1,6 +0,0 @@
-library(hrbrthemes)
-library(tidyverse)
-
-uefalogo <- image_read_svg("uefa2020logo.svg")
-
-grid::grid.raster(uefalogo, x = 1, y = 0.03, just = c('right', 'bottom'), width = unit(3, 'inches'))
